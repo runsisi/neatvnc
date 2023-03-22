484c5b9cbf8c5ac4fbd12f944ed6811b5589c7fb
runsisi <runsisi@hust.edu.cn>
2023-03-22 22:49:50 +0800

    x11: add build target
    Signed-off-by: runsisi <runsisi@hust.edu.cn>

---
+++ examples/x11.cc
@@ -0,0 +1,24 @@
+#include "neatvnc.h"
+
+#include <stdio.h>
+
+#include <X11/Xlib.h>
+#include <X11/extensions/Xcomposite.h>
+
+int main() {
+    Display *dpy = XOpenDisplay(NULL);
+    if (!dpy) {
+        exit(1);
+    }
+
+    int screen = DefaultScreen(dpy);
+    Window root = RootWindow(dpy, screen);
+
+    int major, minor;
+    XCompositeQueryVersion(dpy, &major, &minor);
+    if (major > 0 || minor >= 2) {
+        printf("XComposite available: %d.%d\n", major, minor);
+    }
+
+    return 0;
+}
